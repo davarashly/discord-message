52acffc9a85d35a9a36d0c53ff96b8b6b265a338
Irakli <iraklii.davarashvili@walla.net.il>
2022-08-26 17:30:45 +0300

    Fixed crashing on 2nd attempt

--- src/modules/MessageService.ts
+++ src/modules/MessageService.ts
@@ -18,35 +18,37 @@ export class MessageService {
   }
 
   async processMessages() {
-    await this.getMessages()
+    async function handler(this: MessageService) {
+      await this.getMessages()
 
-    for (const message of this.messages) {
-      const client = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES], loginAsUserAccount: true })
+      for (const message of this.messages) {
+        const client = new Client({ intents: [Intents.FLAGS.DIRECT_MESSAGES], loginAsUserAccount: true })
 
-      client.on("ready", (client) => {
-        console.log(`Logged in as ${client.user.tag}`)
+        client.on("ready", (client) => {
+          const {
+            channelId,
+            data: { content, files }
+          } = message
 
-        const {
-          channelId,
-          data: { content, files }
-        } = message
+          const channel = client.channels.cache.get(channelId) as TextChannel
 
-        const channel = client.channels.cache.get(channelId) as TextChannel
+          channel
+            .send({ files, content })
+            .then(() => {
+              console.group(client.user.tag)
+              console.log("Message was sent!")
+              console.log(message)
+              console.groupEnd()
+            })
+            .catch((e) => console.error(e))
+        })
 
-        channel
-          .send({ files, content })
-          .then(() => {
-            console.group(client.user.tag)
-            console.log("Message was sent!")
-            console.log(message)
-            console.groupEnd()
-          })
-          .catch((e) => console.error(e))
-      })
-
-      client.login(message.token)
+        client.login(message.token)
+      }
     }
 
-    setTimeout(this.processMessages, hrsToMilliseconds(0.51))
+    await handler.bind(this)()
+
+    setTimeout(handler.bind(this), hrsToMilliseconds(0.51))
   }
 }
