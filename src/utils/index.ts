f508346d39358e5a9156bce18bc89149f64727b5
Irakli <iraklii.davarashvili@walla.net.il>
2022-08-27 04:58:43 +0300

    Finished

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,3 +8,11 @@ export const pathResolve = resolve
 export const readFile = (path: string): Buffer => readFileSync(pathResolve(path))
 export const getTsConfig = (): TSConfigJSON => jsonCleanComments(readFile(process.cwd() + "/tsconfig.json").toString())
 export const hrsToMilliseconds = (hrs: number) => hrs * 60 * 60 * 1000
+
+export const transpileHtml = (html: string, data: Record<string, string>) => {
+  for (const key in data) {
+    html = html.replace("#{" + key + "}", data[key].replace('"', "'"))
+  }
+
+  return html
+}
