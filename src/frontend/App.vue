d95a8fd5dc5ac7605b2544c9080084e578b8413d
Irakli <iraklii.davarashvili@walla.net.il>
2023-03-25 17:54:32 +0300

    vite working

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <h1>Hello from Vue s3 with TypeScript and SASS!</h1>
+    <p>{{ sobaka }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-
-
+const sobaka = ''
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   color: #2c3e50;
 }
 </style>
