1b0cfadc040297cee15ce75a337e807b72d62090
irakli <irakli@keyzarjewelry.com>
2023-03-30 03:53:40 +0300

    done

--- config/deleteBuild.ts
+++ config/deleteBuild.ts
@@ -1,4 +1,4 @@
 import { getTsConfig, pathResolve } from "../src/core/utils"
-import fs from "fs"
+import * as fs from "fs"
 
 fs.rmSync(pathResolve(process.cwd(), getTsConfig().compilerOptions!.outDir!), { recursive: true, force: true })
